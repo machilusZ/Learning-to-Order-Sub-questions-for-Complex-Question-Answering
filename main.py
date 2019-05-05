b806032406ae7c25f042c60839253763d88c3d90
Xiang Cheng <xiangc2@illinois.edu>
2019-05-05 12:32:05 -0500

    add rollout

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from tqdm import tqdm
 GAMMA = 0.5
 WORD_EMB_DIM = 300
 NODE_EMB_DIM = 16
-H_DIM = 16
+H_DIM = 32
 T = 3
 NUM_EPOCH = 100
 SOFT_REWARD_SCALE = 0.01
+NUM_ROLL_OUT = 5
 
 parser = argparse.ArgumentParser("main.py")
 parser.add_argument("dataset", help="the name of the dataset", type=str)
@@ -26,7 +27,7 @@ rel_embedding, kg, train, test = load_data(args.dataset, WORD_EMB_DIM)
 word2node = nn.Linear(WORD_EMB_DIM, NODE_EMB_DIM, bias=False)      
 
 # mutihead self-attention 
-attention = Attention(8, NODE_EMB_DIM, H_DIM, 0.01)                
+attention = Attention(4, NODE_EMB_DIM, H_DIM, 0.01)                
 
 # list contains all params that need to optimize
 model_param_list = list(word2node.parameters()) + list(attention.parameters())
@@ -36,7 +37,7 @@ state = State((train[0][1],train[0][2]), kg, WORD_EMB_DIM, word2node, attention,
 input_dim = state.get_input_size()
 num_rel = len(kg.rel_vocab)
 num_entity = len(kg.en_vocab)
-agent = Agent(input_dim, 10, 0.05, 3, num_entity, num_rel, GAMMA, 0.0001, model_param_list)
+agent = Agent(input_dim, 10, 0.1, 2, num_entity, num_rel, GAMMA, 0.0001, model_param_list)
 
 # training loop
 for epoch in range(NUM_EPOCH):
@@ -45,37 +46,38 @@ for epoch in range(NUM_EPOCH):
     correct = 0
     for i in tqdm(range(len(train))):
         # create state from the question
-        state = State((train[i][1],train[i][2]), kg, WORD_EMB_DIM, word2node, attention, rel_embedding)
-        answer = kg.en_vocab[train[i][0]]
-        e0 = state.subgraphs[0][0]
-        agent.policy.init_path(e0)
-        
-        # go for T step
-        for step in range(T):
-            embedded_state = state.get_embedded_state()
-            possible_actions = state.generate_all_possible_actions()
-            action = agent.get_action(embedded_state, possible_actions)
-            r, e = action
-            if step < T-1:
-                agent.hard_reward(0)
-            else:
-                if answer == e:
-                    agent.hard_reward(1)
+        for _ in range(NUM_ROLL_OUT):
+            state = State((train[i][1],train[i][2]), kg, WORD_EMB_DIM, word2node, attention, rel_embedding)
+            answer = kg.en_vocab[train[i][0]]
+            e0 = state.subgraphs[0][0]
+            agent.policy.init_path(e0)
+            
+            # go for T step
+            for step in range(T):
+                embedded_state = state.get_embedded_state()
+                possible_actions = state.generate_all_possible_actions()
+                action = agent.get_action(embedded_state, possible_actions)
+                r, e = action
+                if step < T-1:
+                    agent.hard_reward(0)
                 else:
-                    answer_embedding = state.node_embedding[answer]
-                    e_embedding = state.node_embedding[e]
-                    agent.soft_reward(answer_embedding, e_embedding, SOFT_REWARD_SCALE)
-            state.update(action)
-            #print("step: " + str(step) + ", take action: " + str(action) + "result_subgraphs:" + str(state.subgraphs))
-        
-        # update the policy net and record loss
-        loss, reward, last_reward = agent.update_policy()
-        if last_reward == 1:
-            correct += 1
-        losses.append(loss)
-        rewards.append(reward)
+                    if answer == e:
+                        agent.hard_reward(1)
+                    else:
+                        answer_embedding = state.node_embedding[answer]
+                        e_embedding = state.node_embedding[e]
+                        agent.soft_reward(answer_embedding, e_embedding, SOFT_REWARD_SCALE)
+                state.update(action)
+                #print("step: " + str(step) + ", take action: " + str(action) + "result_subgraphs:" + str(state.subgraphs))
+            
+            # update the policy net and record loss
+            loss, reward, last_reward = agent.update_policy()
+            if last_reward == 1:
+                correct += 1
+            losses.append(loss)
+            rewards.append(reward)
 
-    acc = correct/len(train)
+    acc = correct/(NUM_ROLL_OUT*len(train))
     avg_loss = np.mean(losses)
     avg_reward = np.mean(rewards)
     print("epoch: " + str(epoch) + ", loss: " + str(avg_loss) + ", reward: " + str(avg_reward) + ", acc: " + str(acc))
