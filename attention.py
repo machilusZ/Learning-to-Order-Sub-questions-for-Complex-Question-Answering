754da4b198fbe3845f2d200d5e7016480c034f51
Xiang Cheng <xiangc2@illinois.edu>
2019-05-09 16:30:50 -0500

    fix bug in attention

--- attention.py
+++ attention.py
@@ -3,6 +3,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+'''
+solution for the problem: nn.Module not importing parameters contained in lists
+Adapted from https://discuss.pytorch.org/t/list-of-nn-module-in-a-nn-module/219
+'''
+class ListModule(object):
+    #Should work with all kind of module
+    def __init__(self, module, prefix, *args):
+        self.module = module
+        self.prefix = prefix
+        self.num_module = 0
+        for new_module in args:
+            self.append(new_module)
+
+    def append(self, new_module):
+        if not isinstance(new_module, nn.Module):
+            raise ValueError('Not a Module')
+        else:
+            self.module.add_module(self.prefix + str(self.num_module), new_module)
+            self.num_module += 1
+
+    def __len__(self):
+        return self.num_module
+
+    def __getitem__(self, i):
+        if i < 0 or i >= self.num_module:
+            raise IndexError('Out of bound')
+        return getattr(self.module, self.prefix + str(i))
+
 class Attention(nn.Module):
     def __init__(self, num_of_head, input_dim, output_dim, scale_factor):
         super(Attention, self).__init__()
@@ -10,17 +38,20 @@ class Attention(nn.Module):
         self.output_dim = output_dim
         self.num_of_head = num_of_head
         self.scale_factor = scale_factor
-        self.Wqs = []
-        self.Wks = []
-        self.Wvs = []
+        self.Wqs = ListModule(self, 'wqs')
+        self.Wks = ListModule(self, 'wks')
+        self.Wvs = ListModule(self, 'wvs')
         # TODO: check dim
-        self.softmax = nn.Softmax(dim=0)
-        self.sigmoid = nn.Sigmoid()
 
         for _ in range(num_of_head):
-            self.Wqs.append(nn.Linear(input_dim,output_dim))
-            self.Wks.append(nn.Linear(input_dim,output_dim))
-            self.Wvs.append(nn.Linear(input_dim,output_dim))
+
+            Wq = nn.Linear(input_dim,output_dim)
+            Wk = nn.Linear(input_dim,output_dim)
+            Wv = nn.Linear(input_dim,output_dim)
+
+            self.Wqs.append(Wq)
+            self.Wks.append(Wk)
+            self.Wvs.append(Wv)
 
     def forward(self, X):
         Z = []
@@ -32,10 +63,10 @@ class Attention(nn.Module):
             k = Wk(X)
             v = Wv(X)
             score = self.scale_factor * torch.mm(q,torch.t(k))
-            score = self.softmax(score)
+            score = F.softmax(score, dim=0)
             z = torch.mm(score, v)
             Z.append(z)
         Z = torch.cat(Z, dim=1)
-        return self.sigmoid(Z)
+        return torch.sigmoid(Z)
 
         
