ed220895ce79123f3019e64a1819e50781fc7a0d
Xiang Cheng <xiangc2@illinois.edu>
2019-09-04 15:05:12 -0500

    updates umls for risk eval

---
+++ data/countries/extract.py
@@ -0,0 +1,37 @@
+def load_questions(file_path, line_parser):
+    with open(file_path, 'rb') as fp:
+        line = fp.readline()
+        qs = []
+        while line:
+            question = line_parser(line)
+            if question:
+                qs.append(question)
+            line = fp.readline()
+    return qs
+
+# Parsers, each parser takes in one line of the question file and return (answer, [e1, e2, ..], [r1, r2, ...])
+def parser(line):
+    temp = line.decode("utf-8").strip().split(";")
+    question = temp[0]
+    answer = temp[1]
+    internal_node = temp[2]
+    r1, e1, r2, r3, e2 = question.split("\t")
+    answers = answer.split("\t")[1:]
+    return (answers, e1, e2, r1,r2,r3, internal_node)
+
+train_path = "countries_train.txt"
+test_path = "countries_test.txt"
+test = load_questions(test_path, parser)
+train = load_questions(train_path, parser)
+
+with open("train.txt", 'w') as f1:
+    for question in train:
+        answers, e1, e2, r1,r2,r3, internal_node = question
+        f1.write(answers[0] + "\t" + r1 + "\t" + e1 + "\n")
+    
+with open("test.txt", 'w') as f2:
+    for question in test:
+        answers, e1, e2, r1,r2,r3, internal_node = question
+        f2.write(answers[0] + "\t" + r1 + "\t" + e1 + "\n")
+
+
